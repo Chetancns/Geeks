3f39a879c74f0e66614bd90bcbc0a071ba84e947
Chetancns <chethanns498@gamil.com>
2021-08-28 11:32:34 +0530

    Angular

---
+++ Prototype/Excelcomparer/src/Models/home.model.ts
@@ -0,0 +1,11 @@
+export class JasonData{
+    SourceFile:string|undefined;
+    DestFile:string|undefined;
+    SourceSheetName:string|undefined;
+    DestSheetName:string|undefined;
+    SourceCol:string[]|undefined;
+    DestCol:string[]|undefined;
+    UniqueKeys:string[]|undefined;
+    SelectedRules:string[]|undefined;
+    FlagVariable:string[]|undefined;
+}
